4cd1dfb7a3c43c94dfd4b0da89439213bb803ab8
Sayed Ali Mousavi <sayedaliaqamousavi@gmail.com>
2025-02-10 10:56:21 +0430

    Made the slider responsive

--- frontend/src/app/components/Slider.tsx
+++ frontend/src/app/components/Slider.tsx
@@ -22,13 +22,23 @@ export default function Slider() {
             onMouseLeave={plugin.current.reset}
         >
             <CarouselContent>
-                {Array.from({ length: 3 }).map((_, index) => (
-                    <CarouselItem key={index}>
-                        <div className="w-full h-full">
-                            <img src={`./images/slider/0${index+1}.jpg`} className="w-full sliderHeight object-cover object-center" alt="" />
-                        </div>
-                    </CarouselItem>
-                ))}
+                <CarouselItem key={1}>
+                    <div className="w-full h-full">
+                        <img src={`./images/slider/01.jpg`} className="w-full sliderHeight object-cover object-[70%_30%] md:object-center" alt="" />
+                    </div>
+                </CarouselItem>
+
+                <CarouselItem key={2}>
+                    <div className="w-full h-full">
+                        <img src={`./images/slider/02.jpg`} className="w-full sliderHeight object-cover object-center" alt="" />
+                    </div>
+                </CarouselItem>
+
+                <CarouselItem key={3}>
+                    <div className="w-full h-full">
+                        <img src={`./images/slider/03.jpg`} className="w-full sliderHeight object-cover object-[75%_25%] md:object-[60%_40%] lg:object-center" alt="" />
+                    </div>
+                </CarouselItem>
             </CarouselContent>
             <CarouselPrevious className="bg-white shadow-sm top-[50%] left-0 h-12 w-12 hover:text-primary hover:bg-white" />
             <CarouselNext className="bg-white shadow-sm top-[50%] right-0 h-12 w-12 hover:text-primary hover:bg-white" />
