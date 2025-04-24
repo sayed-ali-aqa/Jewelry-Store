0ada01fab97c423b001eab300a48e7d98d7a4c2b
Sayed Ali Mousavi <sayedaliaqamousavi@gmail.com>
2025-04-24 09:25:46 +0430

    Corrected form integration for shipping methof and payment method

--- frontend/src/app/(routes)/checkout/page.tsx
+++ frontend/src/app/(routes)/checkout/page.tsx
@@ -7,7 +7,7 @@ import { AccountItemType, CheckoutType, SignUpProps } from '@types/allTypes';
 import { signUpForm } from '@utils/actions/auth';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../../../store/slices/authSlice';
 import { Input } from '@/components/ui/input';
@@ -37,6 +37,7 @@ const page = () => {
   const [totalShippingCost, setTotalShippingCost] = useState<number>(0)
 
   const {
+    control,
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
@@ -96,14 +97,14 @@ const page = () => {
 
   const handleShippingMethodChange = (value: string) => {
     setSelectedShippingMethod(value);
-  
+
     const selectedMethod = shippingOptions.find((option) => option.method === value);
-  
+
     if (selectedMethod) {
       setSelectedShippingMethodValue(selectedMethod.price);
     }
   };
-  
+
   const handlePaymentMethodChange = (value: string) => {
     setSelectedPaymentMethod(value);
   };
@@ -255,42 +256,55 @@ const page = () => {
               <div className='bg-white py-6 flex flex-col gap-6'>
                 <h2 className='text-2xl mx-6'>Shipping Methods</h2>
 
-                <RadioGroup value={selectedShippingMethod} onValueChange={handleShippingMethodChange}>
-                  {shippingOptions.map((item, index) => (
-                    <Label
-                      key={index}
-                      htmlFor={item.method}
-                      className="py-3 px-6 flex items-center justify-between space-x-2 transition-all duration-300 cursor-pointer hover:bg-slate-100"
-                    >
-                      <div>
-                        <h3 className="text-lg">{item.method}</h3>
-                        <p className="text-slate-600 font-medium">{item.description}</p>
-                      </div>
-
-                      <div className="flex items-center gap-4">
-                        <span className="text-lg">${item.price.toFixed(2)}</span>
-                        <RadioGroupItem value={`${item.method}`} id={item.method} />
-                      </div>
-                    </Label>
-                  ))}
-                </RadioGroup>
+                <Controller
+                  name="shippingMethod"
+                  control={control}
+                  render={({ field }) => (
+                    <RadioGroup value={field.value} onValueChange={field.onChange}>
+                      {shippingOptions.map((item, index) => (
+                        <Label
+                          key={index}
+                          htmlFor={item.method}
+                          className="py-3 px-6 flex items-center justify-between space-x-2 transition-all duration-300 cursor-pointer hover:bg-slate-100"
+                        >
+                          <div>
+                            <h3 className="text-lg">{item.method}</h3>
+                            <p className="text-slate-600 font-medium">{item.description}</p>
+                          </div>
+                          <div className="flex items-center gap-4">
+                            <span className="text-lg">${item.price.toFixed(2)}</span>
+                            <RadioGroupItem value={item.method} id={item.method} />
+                          </div>
+                        </Label>
+                      ))}
+                    </RadioGroup>
+                  )}
+                />
+                {errors.shippingMethod && <p className="text-red-500 text-sm">{errors.shippingMethod.message}</p>}
               </div>
 
               {/* Payment methods */}
               <div className='bg-white p-6 flex flex-col gap-6'>
                 <h2 className='text-2xl'>Payment Methods</h2>
 
-                <RadioGroup value={selectedPaymentMethod} onValueChange={handlePaymentMethodChange}>
-                  <Label key="Credit" htmlFor="Credit" className="flex items-center space-x-3">
-                    <RadioGroupItem value="Credit" id="Credit" />
-                    <h3 className='text-lg'>Credit Card</h3>
-                  </Label>
+                <Controller
+                  name="paymentMethod"
+                  control={control}
+                  render={({ field }) => (
+                    <RadioGroup value={field.value} onValueChange={field.onChange}>
+                      <Label htmlFor="Credit" className="flex items-center space-x-3">
+                        <RadioGroupItem value="Credit" id="Credit" />
+                        <h3 className='text-lg'>Credit Card</h3>
+                      </Label>
 
-                  <Label key="Paypal" htmlFor="Paypal" className="flex items-center space-x-3">
-                    <RadioGroupItem value="Paypal" id="Paypal" />
-                    <h3 className='text-lg'>Paypal</h3>
-                  </Label>
-                </RadioGroup>
+                      <Label htmlFor="Paypal" className="flex items-center space-x-3">
+                        <RadioGroupItem value="Paypal" id="Paypal" />
+                        <h3 className='text-lg'>Paypal</h3>
+                      </Label>
+                    </RadioGroup>
+                  )}
+                />
+                {errors.paymentMethod && <p className="text-red-500 text-sm">{errors.paymentMethod.message}</p>}
 
                 <div className='flex flex-col gap-2'>
                   <h3 className='font-semibold'>We Accept:</h3>
